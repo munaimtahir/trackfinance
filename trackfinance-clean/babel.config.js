2d65f3ba67b1e786d1a407c79c77da6fd6799d69
unknown <munaim@pmc.edu.pk>
2025-11-13 02:54:48 +0500

    update

--- trackfinance-clean/babel.config.js
+++ trackfinance-clean/babel.config.js
@@ -1,6 +1,10 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // any plugins you need, e.g.
+      // 'expo-router/babel',
+    ],
   };
 };
